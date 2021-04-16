05dbc5bb2abb1ab3258d1ca612a7653a72c9f7ff
Joel Kaye <kayej22@hotmail.com>
2021-04-16 08:21:58 -0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,61 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+function App() {
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="hero-inner">
+          <div className= "hero-content">
+            <div className="hero-content-inner">
+              <h1>
+              <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Relieving the world</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">of it's thirst</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">through soda!</div>
+                </div>
+              </h1>
+              <p>Because nothing quenches thirst like a cold carbonated beverage!</p>
+              <div className="btn-row">
+                <button className="explore-button">
+                explore
+                <div className= "logo">
+                  <img src={'/images/soda_logo.svg'} alt ="company logo"/>
+
+                </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className= "hero-images">
+            <div className="hero-images-inner">
+              <div className="hero-image girl">
+                <img src={'/images/drinkingsoda1.jpg'} alt="girl drinking soda"/>
+              </div>
+              <div className="hero-image group">
+                <img src={'/images/drinkingsoda2.jpg'} alt="girl drinking soda"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+  
+}
+
+
+
+
 export default MyApp
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,58 @@
+import products from '../products.json'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Soda Bros</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Soda Bros
         </h1>
+          <img className={styles.logo} src={'/images/soda_logo.svg'} />
+
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
+            <strong>Cart:</strong> <span className="snipcart-total-price">$0.00</span>
+            </a>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {products.map(product => {
+            return (
+              <div key={product.id} className={styles.card}>
+                <img src={product.image} alt={`Preview of ${product.title}`} />
+                <h3>{ product.title }</h3>
+                <p>{ product.description }</p>
+                <p>$ { product.price }</p>
+                <p>
+                  <button className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-image={product.image}
+                    data-item-name={product.title}
+                    data-item-url="/"
+                    data-item-price={product.price}
+                  >
+                    Add to Cart
+                  </button>
+                </p>
+                </div>
+            );
+          })}
+          
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+       
         </div>
       </main>
 
@@ -60,6 +66,9 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
+      <script async src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js" />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+      <div hidden id="snipcart" data-api-key="ZTc4Y2M1YmUtM2YwZi00MWE5LTljYTEtN2Q0MmVjOThlODU5NjM3NTM3NDk3MzY5MjMwMzcz" />
     </div>
   )
 }
